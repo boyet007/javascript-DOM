3981941442b17d9d448c31fed32d6234c91de898
boyet <boyet007@gmail.com>
2020-08-05 11:32:19 +0700

    08 - Traversing the DOM Part (2)

--- app.js
+++ app.js
@@ -50,3 +50,11 @@ console.log('the parent node is : ', bookList.parentNode);
 console.log('the parent element is : ', bookList.parentElement.parentElement);
 
 console.log(bookList.childNodes);
+
+console.log('book-list next sibling is : ', bookList.nextSibling);
+console.log('book-list next element sibling is : ', bookList.nextElementSibling);
+
+console.log('book-list previous sibling is : ', bookList.previousSibling);
+console.log('book-list previouse element sibling is : ', bookList.previousElementSibling);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br>Too cool for everyone else';
