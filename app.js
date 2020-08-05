36abaffa17e7ad9db6034d44a5c474e9e35a756a
boyet <boyet007@gmail.com>
2020-08-05 15:20:05 +0700

    15 - CheckBoxes & Change Events

--- app.js
+++ app.js
@@ -130,5 +130,15 @@ addForm.addEventListener('submit', function(e){
     list.appendChild(li);
 });
 
+//hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e) {
+    if(hideBox.checked) {
+        list.style.display = 'none';
+    } else {
+        list.style.display = 'initial';
+    }
+})
+
 
 
