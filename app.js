3f4f37350766d7e47a9c65acf005b05bb695b7a4
boyet <boyet007@gmail.com>
2020-08-05 13:44:56 +0700

    09 - events

--- app.js
+++ app.js
@@ -58,3 +58,30 @@ console.log('book-list previous sibling is : ', bookList.previousSibling);
 console.log('book-list previouse element sibling is : ', bookList.previousElementSibling);
 
 bookList.previousElementSibling.querySelector('p').innerHTML += '<br>Too cool for everyone else';
+
+
+var h2 = document.querySelector('#book-list h2');
+console.log(h2);
+
+//event listener
+h2.addEventListener('click', function(e) {
+    console.log(e.target);
+    console.log(e);
+});
+
+
+var btns = document.querySelectorAll('#book-list .delete');
+Array.from(btns).forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li);
+    });
+});
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('navigation to', e.target.textContent , ' was prevented'); 
+});
+
