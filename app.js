f8abd0e55ca216fa849196455f131f9e09ce67a1
boyet <boyet007@gmail.com>
2020-08-05 10:53:24 +0700

    04 - The Query Selector

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-var titles = document.getElementsByClassName('title');
+// var titles = document.getElementsByClassName('title');
 
-Array.from(titles).forEach(title => {
-    console.log(title);
+// Array.from(titles).forEach(title => {
+//     console.log(title);
+// });
+
+const wrap = document.querySelector('#wrapper');
+//console.log(wrap);
+
+const wrp = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(wrp);
+
+//hanya 1 yang di return
+var books = document.querySelector('#book-list li .name');
+// console.log(books);
+
+//semua buku
+books = document.querySelectorAll('#book-list li .name');
+Array.from(books).forEach(function(book) {
+    console.log(book);
 });
