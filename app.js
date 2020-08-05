d3a78e1b765d1d852e09f61bc92636341e2a96c8
boyet <boyet007@gmail.com>
2020-08-05 11:25:19 +0700

    07 - Traversing the DOM (Part 1)

--- app.js
+++ app.js
@@ -11,11 +11,12 @@ const wrp = document.querySelector('#book-list li:nth-child(2) .name');
 // console.log(wrp);
 
 //hanya 1 yang di return
-var books = document.querySelector('#book-list li .name');
+//var books = document.querySelector('#book-list li .name');
 // console.log(books);
 
 //semua buku
 books = document.querySelectorAll('#book-list li .name');
+
 Array.from(books).forEach(function(book) {
     //ganti semua judul dengan text
     //book.textContent = 'Text';
@@ -24,21 +25,28 @@ Array.from(books).forEach(function(book) {
     book.textContent += '{book - title)'
 });
 
-const bookList = document.querySelector('#book-list');
+//const bookList = document.querySelector('#book-list');
 //isi dari html child
 //console.log(bookList.innerHTML);
 //bookList.innerHTML = '<h2>Books and more books...</h2>';
 
 //tambahkan booklist
-bookList.innerHTML += '<p>This is how you Add Html</p>';
+//bookList.innerHTML += '<p>This is how you Add Html</p>';
 
 //nodes
-const banner = document.querySelector('#page-banner');
+//const banner = document.querySelector('#page-banner');
 
-console.log('#page-banner node type is : ', banner.nodeType);
-console.log('#page-banner node name is : ', banner.nodeName);
-console.log('#page-banner has child nodes : ', banner.hasChildNodes());
+//console.log('#page-banner node type is : ', banner.nodeType);
+//console.log('#page-banner node name is : ', banner.nodeName);
+//console.log('#page-banner has child nodes : ', banner.hasChildNodes());
 
-const clonedBanner = banner.cloneNode(false);
+//const clonedBanner = banner.cloneNode(false);
 //kalo true semua isi children diclone, kalo false hanya  element saja
-console.log(clonedBanner);
+//console.log(clonedBanner);
+
+const bookList = document.querySelector('#book-list');
+
+console.log('the parent node is : ', bookList.parentNode);
+console.log('the parent element is : ', bookList.parentElement.parentElement);
+
+console.log(bookList.childNodes);
